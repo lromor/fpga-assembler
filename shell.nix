a58e7f5555b111ba8c11b4d8c8a740cda78f5750
Leonardo Romor <leonardo.romor@gmail.com>
2025-01-16 22:45:32 +0100

    bazel: added prjxray and basic basic bzlmod structure

---
+++ shell.nix
@@ -0,0 +1,27 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  bazel = pkgs.bazel_7;
+  clangTools = pkgs.clang-tools_18;
+in
+pkgs.mkShell {
+  name = "prjxtream";
+  packages = with pkgs; [
+    git
+    bazel
+    jdk
+    bash
+
+    # For clang-tidy and clang-format.
+    clangTools
+
+    # For buildifier, buildozer
+    bazel-buildtools
+  ];
+
+  # Expose as env variables the path to clang tools.
+  CLANG_TIDY = "${clangTools}/bin/clang-tidy";
+  CLANG_FORMAT = "${clangTools}/bin/clang-format";
+
+  # Override .bazelversion. We only care to have bazel 7.
+  USE_BAZEL_VERSION = "${bazel.version}";
+}
