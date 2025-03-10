c2f0c192418d26ce63daccc0beb1f303707835a0
Henner Zeller <h.zeller@acm.org>
2025-03-10 08:38:08 -0700

    Add forgotten file `before-submit.sh`

---
+++ scripts/before-submit.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# Good to run before each submit to catch or repair issues.
+
+cd $(dirname $0)/..
+
+set -e
+
+. <(scripts/run-build-cleaner.sh ...)  # auto-repair issues
+scripts/run-clang-format.sh
+bazel test -c opt ...
+scripts/make-compilation-db.sh
+sh scripts/run-clang-tidy-cached.cc
